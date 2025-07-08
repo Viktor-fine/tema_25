752430a5e73d4b0cf4f10525dbff9110cfeb67ec
Viktor-fine <vik-mailll@mail.ru>
2025-07-09 00:18:53 +0300

    Добавлены ведомости Б2 и Б3

--- vedomosti/urls.py
+++ vedomosti/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'vedomosti'
 urlpatterns = [
     path('', views.b1, name='b1'),
+    path('b2/', views.b2, name='b2'),
+    path('b3/', views.b3, name='b3'),
 ]
--- vedomosti/views.py
+++ vedomosti/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def b1 (request):
     return render (request, 'form_b/b_1.html')
+
+def b2(request):
+    return render (request, 'form_b/b_2.html')
+
+def b3(request):
+    return render (request, 'form_b/b_3.html')
