8066ec47288fa2226f5b203c6f0455f87521608b
Viktor-fine <vik-mailll@mail.ru>
2025-10-10 15:36:21 +0300

    Добавлены ведомости до Б.10

--- vedomosti/urls.py
+++ vedomosti/urls.py
@@ -6,4 +6,11 @@ urlpatterns = [
     path('', views.b1, name='b1'),
     path('b2/', views.b2, name='b2'),
     path('b3/', views.b3, name='b3'),
+    path('b4/', views.b4, name='b4'),
+    path('b5/', views.b5, name='b5'),
+    path('b6/', views.b6, name='b6'),
+    path('b7/', views.b7, name='b7'),
+    path('b8/', views.b8, name='b8'),
+    path('b9/', views.b9, name='b9'),
+    path('b10/', views.b10, name='b10'),
 ]
--- vedomosti/views.py
+++ vedomosti/views.py
@@ -8,3 +8,24 @@ def b2(request):
 
 def b3(request):
     return render (request, 'form_b/b_3.html')
+
+def b4(request):
+    return render (request, 'form_b/b_4.html')
+
+def b5(request):
+    return render (request, 'form_b/b_5.html')
+
+def b6(request):
+    return render (request, 'form_b/b_6.html')
+
+def b7(request):
+    return render (request, 'form_b/b_7.html')
+
+def b8(request):
+    return render (request, 'form_b/b_8.html')
+
+def b9(request):
+    return render (request, 'form_b/b_9.html')
+
+def b10(request):
+    return render (request, 'form_b/b_10.html')
